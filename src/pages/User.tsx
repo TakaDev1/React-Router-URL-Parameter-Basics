caa9d9169d3b8f4f4f30b09996a95f79b3853322
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-25 09:47:16 +0900

    style: Project Adjustment Design

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -4,7 +4,11 @@ import { useParams } from "react-router";
 const User = () => {
   const { id } = useParams<"id">();
 
-  return <div>{!isNaN(Number(id)) ? <p>User ID: {id}</p> : <p>ID Not Found</p>}</div>;
+  return (
+    <div className="text-white text-2xl">
+      {!isNaN(Number(id)) ? <p>User ID: {id}</p> : <p>ID Not Found</p>}
+    </div>
+  );
 };
 
 export default User;
